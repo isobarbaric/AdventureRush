88dc678f4451378d1dcda42f864208797eb541bb
Krish Shah <shahkrish2016@gmail.com>
2022-05-25 13:10:31 -0400

    added some stuff

---
+++ src/adventurerush/Sprite.java
@@ -0,0 +1,23 @@
+package adventurerush;
+
+public class Sprite {
+   
+    private ImageIcon spriteCharacter;
+    private String filepath;
+    private int xCoord, yCord;
+    private int spriteHeight, spriteWidth;
+    private boolean purchasedAlready;
+
+    public Sprite() {
+        // spriteCharacter = something
+        // get some default image for the
+        //
+        
+
+    }
+
+    public Sprite(ImageIcon spriteCharacter, String filePath) {
+         
+    }
+    
+}
