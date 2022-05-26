d892c441b0a43b866ec4a16922791c1859075246
Krish Shah <shahkrish2016@gmail.com>
2022-05-26 18:42:22 -0400

    refactored Menu class a bit + commenting

--- src/adventurerush/Menu.java
+++ src/adventurerush/Menu.java
@@ -6,81 +6,139 @@ import javax.swing.ImageIcon;
 
 public abstract class Menu {
 
+    // attributes of a Menu object
     private String menuName;
     private JFrame menuContent;
     private ArrayList<ImageIcon> assets;
 
+    // constructors 
+
+    /**
+     * Default constructor
+     */
     public Menu() {
         menuName = new String();
         menuContent = new JFrame();
-        assets = new ArrayList<ImageIcon>();
+        assets = new ArrayList<>();
     }
-
+ 
+    /**
+     * Primary constructor
+     * @param menuName name of the Menu object
+     */
     public Menu(String menuName) {
         this(); 
         this.menuName = menuName;
     }
 
+    /**
+     * Secondary constructor
+     * @param menuName name of the Menu object
+     * @param menuContent the JFrame containing the graphical content of the Menu object
+     */
     public Menu(String menuName, JFrame menuContent) {
         this(menuName);
         this.menuContent = menuContent;
     }
 
+    /**
+     * Tertiary constructor
+     * @param menuName name of the Menu object
+     * @param menuContent the JFrame containing the graphical content of the Menu object
+     * @param assets an ArrayList containing the images to be used in displaying this Menu
+     */
     public Menu(String menuName, JFrame menuContent, ArrayList<ImageIcon> assets) {
         this(menuName, menuContent);
         this.assets = assets;
     }
 
-    //Getters
+    // getters
+
+    /**
+     * Getter for the name of the Menu object
+     * @return the name of the Menu object
+     */
     public String getMenuName() {
         return menuName;
     }
 
+    /**
+     * Getter for the JFrame containing the graphical content of the Menu object
+     * @return the JFrame containing the graphical content of the Menu object
+     */
     public JFrame getMenuContent() {
         return menuContent;
     }
 
+    /**
+     * Getter for the ArrayList containing the images to be used in displaying this Menu
+     * @return the ArrayList containing the images to be used in displaying this Menu
+     */ 
     public ArrayList<ImageIcon> getAssets() {
         return assets;
     }
 
-//    public ImageIcon getSpecificAsset(int assetIndex) {
-//
-//    }
+    /**
+     * Alternate getter for the ArrayList containing the images to be used in displaying this Menu
+     * @param index the index of the image to be returned
+     * @return image at the index in the ArrayList
+     * */
+    public ImageIcon getAsset(int index) {
+        return assets.get(index);
+    }
 
-    //Setters
+    // setters
+    
+    /**
+     * Setter for the name of the Menu object
+     * @param menuName the name of the Menu object
+     */
     public void setMenuName(String menuName) {
         this.menuName = menuName;
     }
 
+    /**
+     * Setter for the JFrame containing the graphical content of the Menu object
+     * @param menuContent the JFrame containing the graphical content of the Menu object
+     */
     public void setMenuContent(JFrame menuContent) {
         this.menuContent = menuContent;
     }
 
+    /**
+     * Setter for the ArrayList containing the images to be used in displaying this Menu
+     * @param assets the ArrayList containing the images to be used in displaying this Menu
+     */
     public void setAssets(ArrayList<ImageIcon> assets) {
         this.assets = assets;
     }
 
-    public void setSpecificAsset(int assetIndex, ImageIcon newAsset){
-        
+    /**
+     * Alternate setter for the ArrayList containing the images to be used in displaying this Menu:w
+     * @param asset the image to be added to the ArrayList containing the images to be used in displaying this Menu
+     */
+    public void addAsset(ImageIcon asset) {
+        assets.add(asset);
     }
 
-    //behaviors
-    // public abstract void loadMenuFunctionality();
-    
-    public void quitMenu(){
-        
-    }
-        
-    //standard methods
-    
-    // public boolean equals(Menu otherMenu){
-        
-    // }
+    // standard methods
 
-    // standard Java toString() method
+    /**
+     * Standard Java toString() methods
+     * @return toDisplay a string containing information about the current Menu object
+     */
     public String toString() {
-        return "menuName=" + menuName + ", menuContent=" + menuContent + ", assets=" + assets;
+        String toDisplay = "Menu Name: " + menuName + ", Menu Type: ";
+        return toDisplay;
     }
     
+    /**
+     * Standard Java equals() method
+     * @param otherMenu the object to be compared to the current Menu object
+     * @return true if the current Menu object is equal to the Menu object passed as a parameter
+     */
+    public boolean equals(Menu otherMenu) {
+        return menuName.equals(otherMenu.getMenuName()) && menuContent.equals(otherMenu.getMenuContent()) && assets.equals(otherMenu.getAssets());
+    }
+
 }
