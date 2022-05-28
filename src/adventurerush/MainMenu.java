b36f5c84101174ced1e0cb884b1c7d0d518fbd5c
Krish Shah <shahkrish2016@gmail.com>
2022-05-27 22:17:54 -0400

    created MainMenuWindow class and integrated it with MainMenu

--- src/adventurerush/MainMenu.java
+++ src/adventurerush/MainMenu.java
@@ -6,31 +6,63 @@ import javax.swing.JFrame;
 
 public class MainMenu {
   
+    // attributes of a MainMenu object
     private String menuName;
     private JFrame menuContent;
     private ArrayList<ImageIcon> assets;
     private boolean buttonPressed;
     
-    
-    public MainMenu(String menuName){
-        this.menuName = menuName;
-        
+    /**
+     * Default constructor 
+     */
+    public MainMenu() {
+        menuName = new String();
+        menuContent = new JFrame();
+        assets = new ArrayList();
+        buttonPressed = false;
     }
     
+    /**
+     * Primary constructor
+     * @param menuName 
+     */
+    public MainMenu(String menuName){
+        this.menuName = menuName;    
+    }
+    
+    /**
+     * Secondary constructor
+     * @param menuName
+     * @param menuContent 
+     */
     public MainMenu(String menuName, JFrame menuContent){
         this(menuName);
         this.menuContent = menuContent;
     }
   
+    /**
+     * Tertiary constructor
+     * @param menuName
+     * @param menuContent
+     * @param assets 
+     */
     public MainMenu(String menuName, JFrame menuContent, ArrayList<ImageIcon> assets){
         this(menuName, menuContent);
         this.assets = assets;
     }
      
+    /**
+     * Getter for the buttonPressed attribute
+     * @return 
+     */
     public boolean getButtonPressed(){
         return buttonPressed;
     }
    
+    /**
+     * Setter for the buttonPressed attribute
+     * @param buttonPressed 
+     */
     public void setButtonPressed(boolean buttonPressed){
         this.buttonPressed = buttonPressed;
     }
