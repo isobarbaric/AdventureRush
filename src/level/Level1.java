2d3068e7f30e784c6a63e91b77300c4b317517bc
Krish Shah <shahkrish2016@gmail.com>
2022-06-08 12:44:36 -0400

    cleaned up some code rite b4 class

--- src/level/Level1.java
+++ src/level/Level1.java
@@ -18,9 +18,8 @@ public class Level1 extends Level {
         if (currentLevelShapes == null) {
             currentLevelShapes = super.getCurrentLevelShapes();
         }
-        // currentLevelShapes.add(new Shape(0, width, 0, height, "N/A", Color.GREEN));
         currentLevelShapes.add(new Shape(800, 600, 600, 800, "N/A", Color.BLUE));   
-        // currentLevelShapes.add(new Shape(0, width, height-100, height, "N/A", Color.GREEN));
+        currentLevelShapes.add(new Shape(200, 400, 400, 600, "N/A", Color.GREEN));
     }
         
 }
--- src/level/Level2.java
+++ src/level/Level2.java
@@ -18,9 +18,6 @@ public class Level2 extends Level {
         if (currentLevelShapes == null) {
             currentLevelShapes = super.getCurrentLevelShapes();
         }
-        // currentLevelShapes.add(new Shape(0, width, 0, height, "N/A", Color.GREEN));
-        currentLevelShapes.add(new Shape(800, 600, 600, 800, "N/A", Color.BLUE));   
-        // currentLevelShapes.add(new Shape(0, width, height-100, height, "N/A", Color.GREEN));
     }
         
 }
